82070c8c65d2754be7b3b984299e7098516195de
diegoapb <parra.diego@correounivalle.edu.co>
2021-11-10 14:16:51 -0500

    Initial commit

---
+++ test/demo-cdk-api-lambda.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as DemoCdkApiLambda from '../lib/demo-cdk-api-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-cdk-api-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoCdkApiLambda.DemoCdkApiLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
