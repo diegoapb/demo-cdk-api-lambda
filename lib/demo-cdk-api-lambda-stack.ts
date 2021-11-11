d3d4d08f064adb8aa66e823bde05e9e503d4ad74
diegoapb <parra.diego@correounivalle.edu.co>
2021-11-11 00:55:58 -0500

    Organize Structure folders

--- lib/demo-cdk-api-lambda-stack.ts
+++ lib/demo-cdk-api-lambda-stack.ts
@@ -1,34 +1,30 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 //import Function
-import { Code, Function as LambdaFunction, Runtime } from '@aws-cdk/aws-lambda';
+import { Code, Function as LambdaFunction, Runtime } from "@aws-cdk/aws-lambda";
 //import Bucket
-import { Bucket } from '@aws-cdk/aws-s3';
+import { Bucket } from "@aws-cdk/aws-s3";
 //import API
-import { RestApi, LambdaIntegration } from '@aws-cdk/aws-apigateway';
+import { RestApi, LambdaIntegration } from "@aws-cdk/aws-apigateway";
 
 //import utils path
-import { join } from 'path';
+import { join } from "path";
 export class DemoCdkApiLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    //Bucket
-    const bucket = new Bucket(this, 'HelloBucket');
-
+    
     //Handler
-    const handler = new LambdaFunction(this, 'HelloHandler', {
+    const handler = new LambdaFunction(this, "HelloHandler", {
       runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, '../lambda')),
-      handler: 'handler.sayHello',
+      code: Code.fromAsset(join(__dirname, "../services")),
+      handler: "greeting.sayHello",
     });
 
     //API Gateway
-    const api = new RestApi(this, 'HelloRestApi')
+    const api = new RestApi(this, "HelloRestApi");
 
     //Integration POST /hello with Lambda
     api.root
-    .resourceForPath('/hello')
-    .addMethod('POST', new LambdaIntegration(handler));
-
+      .resourceForPath("/hello")
+      .addMethod("POST", new LambdaIntegration(handler));
   }
 }
